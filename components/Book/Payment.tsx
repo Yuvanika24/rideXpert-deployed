387e2d2e270f652c9c3d97dc904ae96845df6adb
Yuvanika <yuvanika@pop-os.localdomain>
2024-05-11 17:55:18 +0530

    fixed payment

--- components/Book/Payment.tsx
+++ components/Book/Payment.tsx
@@ -10,8 +10,8 @@ function Payment() {
     <div className='mt-3'>
         <h2 className='font-semibold'>Payment Method</h2>
         <div className='grid grid-cols-4'>
-            {paymentMethod.map((value,index)=>(
-                <div 
+            {paymentMethod.map((value:any,index:number)=>(
+                <div key={index}
                     className={`m-2 px-4 hover:border-blue-500 hover:border-[2px] rounded-md cursor-pointer ${index==selectedPayment ? 'border-[2px] border-blue-500 rounded-md' : null }`}
                     onClick={()=>setSelectedPayment(index)} 
                 >   
