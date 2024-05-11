f49f0efb14a4350731d4ce50250c41744d7a85fb
Yuvanika <yuvanika@pop-os.localdomain>
2024-05-11 16:45:26 +0530

    fixed deploy issues

--- components/Maps/MapBox.tsx
+++ components/Maps/MapBox.tsx
@@ -21,28 +21,7 @@ function MapBox() {
   const{destCoord,setDestCoord}=useContext(DestCoordContext);
 
   const{directionData,setDirectionData}=useContext(DirectionDataContext);
-
-  useEffect(()=>{
-      if(sourceCoord){
-        mapRef.current?.flyTo({
-          center:[sourceCoord.lon, sourceCoord.lat],
-          duration:2500
-        })
-      }
-  },[sourceCoord])
-
-  useEffect(()=>{
-    if(destCoord){
-      mapRef.current?.flyTo({
-        center:[destCoord.lon, destCoord.lat],
-        duration:1000
-      })
-    }
-    if(sourceCoord && destCoord){
-      getDirectionRoute()
-    }
-  },[destCoord])
-
+  
   const getDirectionRoute=async()=>{
     const response= await fetch(MAPBOX_DRIVING_ENDPOINT+
       sourceCoord.lon+','+ sourceCoord.lat+';'+
@@ -58,6 +37,27 @@ function MapBox() {
     const result=await response.json()
     setDirectionData(result)
   }
+  useEffect(()=>{
+      if(sourceCoord){
+        mapRef.current?.flyTo({
+          center:[sourceCoord.lon, sourceCoord.lat],
+          duration:2500
+        })
+      }
+  },[sourceCoord])
+  
+  useEffect(()=>{
+    if(destCoord){
+      mapRef.current?.flyTo({
+        center:[destCoord.lon, destCoord.lat],
+        duration:1000
+      })
+    }
+    if(sourceCoord && destCoord){
+      getDirectionRoute()
+    }
+  },[destCoord, sourceCoord, getDirectionRoute])
+
 
  
   return (
--- components/Maps/Markers.tsx
+++ components/Maps/Markers.tsx
@@ -1,5 +1,5 @@
-import React, {useContext} from 'react'
-import {Marker} from 'react-map-gl'
+import React, { useContext } from 'react'
+import { Marker } from 'react-map-gl'
 import { UserLocationContext } from '@/context/UserLocationContext';
 import { SourceCoordContext } from '@/context/SourceCoordContext'
 import { DestCoordContext } from '@/context/DestCoordContext'
@@ -11,35 +11,38 @@ function Markers() {
   
   return (
     <div>
-      {/*User Marker*/}
+      {/* User Marker */}
+      {userLocation && (
         <Marker 
-            latitude={userLocation?.lat}
-            longitude={userLocation?.lon}
-            anchor='bottom'>
-
-            <img src='./pin.png'
-            className='w-8 h-8'/>
+          latitude={userLocation.lat}
+          longitude={userLocation.lon}
+          anchor='bottom'
+        >
+          <img src='./pin.png' className='w-8 h-8' alt='User location'/>
         </Marker>
-        
-        {/*Source Marker*/}
-        {sourceCoord.length!=0 ? <Marker 
-            latitude={sourceCoord?.lat}
-            longitude={sourceCoord?.lon}
-            anchor='bottom'>
-
-            <img src='./pin.png'
-            className='w-8 h-8'/>
-        </Marker> : null}
-        
-        {/*Destination Marker*/}
-        {destCoord.length!=0 ? <Marker 
-            latitude={destCoord?.lat}
-            longitude={destCoord?.lon}
-            anchor='bottom'>
-
-            <img src='./pin.png'
-            className='w-8 h-8'/>
-        </Marker> : null}
+      )}
+      
+      {/* Source Marker */}
+      {sourceCoord.length !== 0 && (
+        <Marker 
+          latitude={sourceCoord.lat}
+          longitude={sourceCoord.lon}
+          anchor='bottom'
+        >
+          <img src='./pin.png' className='w-8 h-8' alt='Source location'/>
+        </Marker>
+      )}
+      
+      {/* Destination Marker */}
+      {destCoord.length !== 0 && (
+        <Marker 
+          latitude={destCoord.lat}
+          longitude={destCoord.lon}
+          anchor='bottom'
+        >
+          <img src='./pin.png' className='w-8 h-8' alt='Destination location'/>
+        </Marker>
+      )}
     </div>
   )
 }
